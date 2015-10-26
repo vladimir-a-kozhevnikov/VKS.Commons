4e4bbda723f615f5e3bf14f70f2347dbb0b07abc
Vladimir A. Kozhevnikov <vkozhevnikov@serve-u.net>
2015-10-26 23:12:25 +0300

    Defined some basic abstractions and interfaces for measurements system.

---
+++ VKS.Measures/Measure.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace VKS.Measures
+{
+    /// <summary>
+    /// Abstract class that defines basic requirements and rules for all measures.
+    /// </summary>
+    /// <typeparam name="TPrimitive">The type of the measure's underlying primitive.</typeparam>
+    public abstract class Measure<TPrimitive> : INotifyPropertyChanged, IFormattable where TPrimitive : struct, IComparable
+    {
+        /// <summary>
+        /// The back-storage field that contains actual value of measure.
+        /// </summary>
+        private TPrimitive FInnerValue;
+
+        /// <summary>
+        /// Gets or sets the inner representation of the measure value.
+        /// </summary>
+        /// <value>
+        /// The inner value of the measure.
+        /// </value>
+        protected virtual TPrimitive InnerValue
+        {
+            get { return FInnerValue; }
+            set
+            {
+                if(FInnerValue.CompareTo(value) != 0)
+                {
+                    OnPropertyChanged();
+                    FInnerValue = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Occurs when property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <param name="format">The specified format sequence.</param>
+        /// <param name="formatProvider">The format provider for instance.</param>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public abstract string ToString(string format, IFormatProvider formatProvider);
+
+        /// <summary>
+        /// Fires the <see cref="PropertyChanged"/> event on this instance.
+        /// </summary>
+        /// <param name="propertyName">Name of the changed property.</param>
+        protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            var _Handler = PropertyChanged;
+            if (_Handler != null)
+                _Handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+    }
+}
---
+++ VKS.Measures/PhysicalMeasure.cs
@@ -0,0 +1,26 @@
+﻿using VKS.Measures.Systems;
+using System.ComponentModel;
+
+namespace VKS.Measures
+{
+    /// <summary>
+    /// Abstract class that defines behavior for all physical measures in library (length, mass, force, pressure etc).
+    /// </summary>
+    public abstract class PhysicalMeasure: Measure<double>
+    {
+        /// <summary>
+        /// Converts from primitive value in specified <see cref="IMeasurementSystem"/> to an inner representation of measure.
+        /// </summary>
+        /// <param name="sourceSystem">The source system of measurement (i.e. Avoirdupois, ISoU etc).</param>
+        /// <param name="sourceValue">The value of primitive.</param>
+        protected abstract void ConvertFromPrimitive(double sourceValue, IMeasurementSystem sourceSystem);
+
+        /// <summary>
+        /// Converts from inner representation to primitive value (<see cref="System.Double"/>).
+        /// </summary>
+        /// <param name="targetSystem">The target system of measurements.</param>
+        /// <returns>The primitive value that contains the measure representation in specified measurement system.</returns>
+        protected abstract double ConvertToPrimitive(IMeasurementSystem targetSystem);
+
+    }
+}
