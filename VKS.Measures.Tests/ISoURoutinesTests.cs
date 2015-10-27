7e789540960008f5cc54d19b529813b3bca6997a
Vladimir A. Kozhevnikov <vladimir.a.kozhevnikov@serve-u.net>
2015-10-27 21:49:45 +0300

    Tests added

---
+++ VKS.Measures.Tests/ISoURoutinesTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+using VKS.Measures;
+using VKS.Measures.RC;
+
+namespace VKS.Measures.Tests
+{
+	[TestFixture]
+	public class ISoURoutinesTests
+	{
+		[TestCase(double.NaN, 0, ExpectedException = typeof(ArgumentOutOfRangeException))]
+		[TestCase(double.NegativeInfinity, 0)]
+		[TestCase(double.PositiveInfinity, 0)]
+		[TestCase(1D, 0)]
+		[TestCase(1350.2345323434534D, 3)]
+		[TestCase(0.024D, -2)]
+		[TestCase(0.0000000345D, -8)]
+		public void Test_GetScalePower(double value, int expectedResult)
+		{
+			Assert.AreEqual(expectedResult, InternationalSystemOfUnits.GetScalePower(value));
+		}
+
+		[TestCase(1, "en-US")]
+		[TestCase(5, "ru-RU")]
+		[TestCase(5, "fr-FR")]
+		public void Test_GetScalePrefix(int scale, string cultureCode = "en-US")
+		{
+			Console.WriteLine(InternationalSystemOfUnits.GetScalePrefix(scale));
+		}
+	}
+}
